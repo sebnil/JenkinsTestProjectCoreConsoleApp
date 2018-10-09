8f242b7343dbb072b6f479a8fd4aade9e1b86d5b
Sebastian Nilsson <sebnil@gmail.com>
2018-10-09 13:40:35 +1000

    Add project files.

---
+++ JenkinsTestProjectCoreConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JenkinsTestProjectCoreConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
